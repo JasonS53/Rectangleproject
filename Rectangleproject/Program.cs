932cb577dc623283722c2ee2904f7f792e711b0e
Jason <j_schubb53@yahoo.com>
2021-04-05 11:24:10 -0400

    first commit

---
+++ Rectangleproject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rectangleproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of the rectangle?");
+            string length = Console.ReadLine();
+            double L = double.Parse(length);
+            Console.WriteLine("What is the width of the rectangle?");
+            string width = Console.ReadLine();
+            double W = double.Parse(width);
+            double area = L * W;
+            double parameter = (L * 2) + (W * 2);
+            Console.WriteLine("The area of the rectangle is " + area + " and the parameter is " + parameter + ".");
+        }
+    }
+}
